c06c1bbc873603a78a23a58b2a0804187df0cbd8
Rute Cintra <ruteocintra@gmail.com>
2025-01-19 20:25:46 -0300

    fix: relationships model

--- backend/src/models/relationships.py
+++ backend/src/models/relationships.py
@@ -1,30 +1,36 @@
 from django.db import models
 from .appmodel import RelationshipModel
+from .orders import Orders
+from .ordershippings import OrderShippings
+from .orderproducts import OrderProducts
+from .products import Products
+from .categories import Categories
+from .logistics import Logistics
 
 class OrdersOrderShippings(RelationshipModel):
-    _out = models.ForeignKey('orders.Orders', on_delete=models.CASCADE)
-    _in = models.ForeignKey('ordershippings.OrderShippings', on_delete=models.CASCADE)
+    _out = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    _in = models.ForeignKey(OrderShippings, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'orders_ordershippings'
 
 class OrderProductsOrderShippings(RelationshipModel):
-    _out = models.ForeignKey('orderproducts.OrderProducts', on_delete=models.CASCADE)
-    _in = models.ForeignKey('ordershippings.OrderShippings', on_delete=models.CASCADE)
+    _out = models.ForeignKey(OrderProducts, on_delete=models.CASCADE)
+    _in = models.ForeignKey(OrderShippings, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = 'ordersproducts_ordershippings'
+        db_table = 'orderproducts_ordershippings'
 
 class ProductsCategories(RelationshipModel):
-    _out = models.ForeignKey('products.Products', on_delete=models.CASCADE)
-    _in = models.ForeignKey('categories.Categories', on_delete=models.CASCADE)
+    _out = models.ForeignKey(Products, on_delete=models.CASCADE)
+    _in = models.ForeignKey(Categories, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'products_categories'
 
 class ProductsLogistics(RelationshipModel):
-    _out = models.ForeignKey('products.Products', on_delete=models.CASCADE)
-    _in = models.ForeignKey('logistics.Logistics', on_delete=models.CASCADE)
+    _out = models.ForeignKey(Products, on_delete=models.CASCADE)
+    _in = models.ForeignKey(Logistics, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'products_logistics'
